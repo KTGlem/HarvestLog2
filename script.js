471809c426282fef008409e9e8ad993b82b540bf
KTGlem <info@coldspringsorganics.com>
2025-05-25 09:23:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURATION
 // --------------------
 const SHEET_DATA_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSx6K7uggH34_CSFemM_Tr9Kjg4laVdtuU3hEAj6Ky4ispV6lUOp0S5bteIj6j8-hWcyiGhWlzMIbIP/pub?gid=0&single=true&output=csv';
-const FORM_POST_URL = 'https://script.google.com/macros/s/AKfycbyktHeJIpD2Pmq8rT3Af9lz1kSAatGw9KC6KdXZzuslCuaQuwlUurpoCHzkbZkmJA96/exec';
+const FORM_POST_URL = 'https://script.google.com/macros/s/AKfycbymx1PgoAYYYsBQFnq04hRy1RmjbudrzGypDnW18NTdy2dlHhIpEFBvrXuJExqC2NSZ/exec';
 
 let currentRow = null;
 let allTasks = []; // Correctly initialized
